564026a2be62afb226427d02a3e99a48861bda9f
Malcolm Gillies <malcolm.gillies@unsw.edu.au>
2021-10-15 09:33:44 +1100

    Added weighting to best line fit for SAS sort benchmark

--- sort-bench.R
+++ sort-bench.R
@@ -6,10 +6,9 @@ b<-data.frame(n=c(1,2,4,8,10), t=c(10,24,69,201,297))
 pdf("sort-bench.pdf", width=6, height=3)
 
 b %>%
-	ggplot(aes(x=n, y=t)) + geom_point() +
+	ggplot(aes(x=n, y=t, weight=1/sqrt(t))) + geom_point() +
 	xlab("n (M records)") + ylab("t (seconds)") +
-	geom_smooth(method="lm", se=TRUE, fullrange=FALSE, level=0.95,
-		formula = y ~ x + I(x*log(x))) %>%
-	print
+	geom_smooth(method="lm", se=TRUE, fullrange=TRUE, level=0.95,
+		formula = y ~ x + I(x*log(x)))
 
 dev.off()
