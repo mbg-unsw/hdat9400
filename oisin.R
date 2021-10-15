6cd9fe7c3c14ed83b569d85466c553efd758cc0b
Malcolm Gillies <malcolm.gillies@unsw.edu.au>
2021-10-15 15:32:16 +1100

    Made graph narrower so text can fit alongside on slide

--- oisin.R
+++ oisin.R
@@ -4,12 +4,12 @@ library(dplyr)
 b<-read.table("ref/oisin.csv", header=T)
 
 
-pdf("ref/oisin.pdf", width=6, height=3)
+pdf("ref/oisin.pdf", width=5, height=3)
 
 b %>%
 	ggplot(aes(x=obs/1e6, y=time, weight=1/sqrt(time))) + geom_point() +
 	xlab("n (M records)") + ylab("t (seconds)") +
 	geom_smooth(method="lm", se=TRUE, fullrange=TRUE, level=0.95,
-		formula = y ~ x + I(x^2))
+		formula = y ~ I(x^2))
 
 dev.off()
