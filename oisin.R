c4af5451358a54e69b94f69685cec288c4dc90fd
Malcolm Gillies <malcolm.gillies@unsw.edu.au>
2021-10-15 15:12:48 +1100

    Added plot of time complexity for Oisin's ICU monitoring example

---
+++ oisin.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+library(dplyr)
+
+b<-read.table("ref/oisin.csv", header=T)
+
+
+pdf("ref/oisin.pdf", width=6, height=3)
+
+b %>%
+	ggplot(aes(x=obs/1e6, y=time, weight=1/sqrt(time))) + geom_point() +
+	xlab("n (M records)") + ylab("t (seconds)") +
+	geom_smooth(method="lm", se=TRUE, fullrange=TRUE, level=0.95,
+		formula = y ~ x + I(x^2))
+
+dev.off()
